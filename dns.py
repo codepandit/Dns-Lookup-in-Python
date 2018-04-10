7b8ce6a98a4c05f96e7e906f6c6d6df87a224fdb
CODEPANDIT\nikhi <nikhil.mehral@hotmail.com>
2018-04-10 14:53:56 +1000

    Almost Working but lot needs to be done

--- dns.py
+++ dns.py
@@ -1,163 +1,51 @@
-import socket
-import sys
 import struct
+import socket
+import argparse
+import dnslib
 
-# construct the DNS query
-def dnsquery(domain):
-    d = ""
-    for a in domain.split('.'):
-        d = d + struct.pack("!b" + str(len(a)) + b"s", len(a), a)
+#@author Patrick Mathieu / @pathetiq
+# This code is an update from: 
+# DNS packet struct packet taken from: http://stackoverflow.com/questions/24814044/having-trouble-building-a-dns-packet-in-python 
+# User: http://stackoverflow.com/users/3850901/monkeyba
+#
 
-    l1 = "\x41\x41"
-    l2 = "\x01\x00"
-    l3 = "\x00\x01"
-    l4 = "\x00\x00"
-    l5 = "\x00\x00"
-    l6 = "\x00\x00"
-    header = l1 +l2 + l3 + l4 + l5 + l6
-    q = d + "\x00\x00\x01\x00\x01"
-    m = header + q
-    #print "DNS query: ", m.encode("hex")
-    return m
+class DnsPacketBuilder:
+        def __init__(self):
+                pass
 
-# send the DNS query to the DNS server using UDP
-def Conn (ser, q):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(5)
-        sock.sendto(q, ser)		
-        data, addr = sock.recvfrom(1024)
+        def build_packet(self, url):
+                packet = struct.pack(">H", 12049)  # Query Ids (Just 1 for now)
+                packet += struct.pack(">H", 256)  # Flags
+                packet += struct.pack(">H", 1)  # Questions
+                packet += struct.pack(">H", 0)  # Answers
+                packet += struct.pack(">H", 0)  # Authorities
+                packet += struct.pack(">H", 0)  # Additional
+                
+		#@TODO all message should be in the form of a IP address with padding and translation to 1 to 255
+                split_url = url.split(".")
+                for part in split_url:
+                        #parts = part.encode('utf-8')
+                        packet += struct.pack("B", len(part))
+                        for byte in part:
+                            packet += struct.pack("c", byte.encode('utf-8'))
+                
+                packet += struct.pack("B", 0)  # End of String
+                packet += struct.pack(">H", 1)  # Query Type
+                packet += struct.pack(">H", 1)  # Query Class
+                return packet
+                
 
-    except Exception as e:
-        print ("No response from server", ser,e)
-        print ("------------------------------------------------------")
-        print ("")
-        sock.close()
-        sys.exit()
-	
-	# get the messages from the server	
-    severmessage = data.split(',', 0)
-	#print "server message: ", severmessage
-	
-    reply = str(severmessage[0].encode("hex"))
-	#print "Sever reply: ", reply	
-		
-	# get the RCODE field from the sever message
-    rcode = reply[:8]
-    rcode = rcode[7:]
-    r = int(rcode)
-	
-	# check the RCODE with error detection 
-    if (r == 0):	# no error, get the ip address	
-		# check the ANCOUNT to fine the number of answers
-        ancount = reply[:16]
-        ancount = ancount[12:]
-        count = int(ancount)
-        print ("Number of Answers: ", count)
-        print ("")
-		
-		# get the answer from the server reply
-        answer = reply[2*len(q):]
-        name = domainName
-        for i in range(0, count):			
-            print ("Answer ", i+1, ": ")
-            
-            # get the owner name from the answer
-            n = answer[:4]
-            #print "NAME: ", name
-            answer = answer[4:]
-            
-            # get the TYPE from the answer
-            t = answer[:4]
-            if (t == "0001"):
-                print ("TYPE: A")
-            elif (t == "0005"):
-                print ("TYPE: CNAME")
-            else:
-                print ("TYPE: ", t)
-            answer = answer[4:]
-            
-            # get the CLASS from the answer
-            c = answer[:4]
-            if (c == "0001"):
-                print ("CLASS: IN")
-            else:
-                print ("CLASS: ", c)
-            answer = answer[4:]
-            
-            # get the TTL from the answer
-            ttl = answer[:8]
-            print ("TTL: ", int(ttl, 16), "seconds")
-            answer = answer[8:]
-            
-            # get the RDLENGTH from the answer
-            l = answer[:4]
-            print ("RDLENGTH: ", int(l, 16))
-            answer = answer[4:]
-            
-            # get the RDATA from the answer
-            rdata = answer[:int(l, 16)*2]
-            answer = answer[int(l, 16)*2:]
-            
-            if (t == "0001"): # get the IP from the RDATA if the TYPE is A			
-                a = int(rdata, 16)		
-                ip4 = a & 0xff
-                a = a >> 8
-                ip3 = a & 0xff
-                a = a >> 8
-                ip2 = a & 0xff
-                a = a >> 8
-                ip1 = a & 0xff
-                print ("IP: " + str(ip1) + "." + str(ip2) + "." + str(ip3) + "." + str(ip4))
-                print ("")
-            else: # get the CNAME from the RDATA if the TYPE is CNAME				
-                cname = ""
-                rdata = rdata[2:]
-                # print the CNAME
-                for i in range(int(l, 16)-3):
-                    #print "RDATA: ", rdata					
-                    n = rdata[:2]
-                    n = int(n, 16)	
-                    rdata = rdata[2:]									
-                    if n <= 32:
-                        cname = cname + "."											
-                    cname = cname + chr(n)					
-                print ("CNAME: ", cname)
-                print ("")
-                #name = cname
-    elif (r == 1):
-        # error
-        print ("*** Format error: the name server was unable to interpret the query.")
-    elif (r == 2):
-        # error
-        print ("*** Server failure: the name server was unable to process this query due to a problem with the name server.")
-    elif (r == 3):
-        # error
-        print ("*** None exist domains: server can not find answer.")
-    elif (r == 4):
-        # error
-        print ("*** Not Implemented: the name server does not support the requested kind of query.")
-    elif (r == 5):
-        # error
-        print ("*** Server Refused.")
-    else:
-        # error
-        print ("*** Other errors")
+   
+# Sending the packet
+builder = DnsPacketBuilder()
+packet = builder.build_packet("apple.com.au")
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 8888))
+sock.settimeout(2)
+sock.sendto(bytes(packet), ("208.67.222.222", 53))
+print("Packet Sent")
+data, addr = sock.recvfrom(1024)
+result = dnslib.DNSRecord().parse(data).format()
 
-    print ("------------------------------------------------------")
-    print ("")
-    sock.close()
-	
-	
-if __name__ == '__main__':
-
-	serverIP = sys.argv[1]
-	domainName = sys.argv[2]
-	server = (serverIP, 53)
-	print ("")
-	print ("-------------------------------------------------")
-	print ("DNS server IP: \t", server)
-	print ("Domain Name: \t", domainName)
-	print ("")
-	query = dnsquery(domainName)
-	Conn(server, query)
+print(result)
+sock.close()
