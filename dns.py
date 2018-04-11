aa99935f9350eabc9e9bd9427b501372952f1d28
Nikhil <nikhil.mehral@hotmail.com>
2018-04-11 13:33:23 +1000

    Now need to pass the arguments

--- dns.py
+++ dns.py
@@ -15,11 +15,15 @@ class DnsPacketBuilder:
 
         def build_packet(self, url):
                 packet = struct.pack(">H", 12049)  # Query Ids (Just 1 for now)
-                packet += struct.pack(">H", 256)  # Flags
+                #packet = struct.pack(">H", 23000)  # Query Ids for MX
+                #packet = struct.pack(">H", 1)  # Query Ids for CNAME
+                #packet = struct.pack(">H", 2)  # Query Ids for Inverse Query
+                packet += struct.pack(">H", 256)  # Flags AA RA RD TC
                 packet += struct.pack(">H", 1)  # Questions
                 packet += struct.pack(">H", 0)  # Answers
                 packet += struct.pack(">H", 0)  # Authorities
                 packet += struct.pack(">H", 0)  # Additional
+                
                 
 		#@TODO all message should be in the form of a IP address with padding and translation to 1 to 255
                 split_url = url.split(".")
@@ -30,19 +34,20 @@ class DnsPacketBuilder:
                             packet += struct.pack("c", byte.encode('utf-8'))
                 
                 packet += struct.pack("B", 0)  # End of String
-                packet += struct.pack(">H", 1)  # Query Type
+                packet += struct.pack(">H", 12)  # Query Type 2-NS, 15-MX, 5-CNAME, 12-PTR
                 packet += struct.pack(">H", 1)  # Query Class
+                print(packet)
                 return packet
                 
 
    
 # Sending the packet
 builder = DnsPacketBuilder()
-packet = builder.build_packet("apple.com.au")
+packet = builder.build_packet("130.102.71.160")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('', 8888))
 sock.settimeout(2)
-sock.sendto(bytes(packet), ("208.67.222.222", 53))
+sock.sendto(bytes(packet), ("192.168.1.1", 53))
 print("Packet Sent")
 data, addr = sock.recvfrom(1024)
 result = dnslib.DNSRecord().parse(data).format()
